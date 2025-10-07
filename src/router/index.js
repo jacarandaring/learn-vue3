2575d92227db28bec9b47957fac8ea6c800eeda4
JY <81917385+jacarandaring@users.noreply.github.com>
2025-10-07 17:38:25 +0900

    [실전] vue-router 실습 (게시물 조회/추가/수정)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView';
 import AboutView from '@/views/AboutView';
+import PostListView from '@/views/posts/PostListView';
+import PostCreateView from '@/views/posts/PostCreateView';
+import PostDetailView from '@/views/posts/PostDetailView';
+import PostEditView from '@/views/posts/PostEditView';
 
 const routes = [
     {
@@ -12,6 +16,26 @@ const routes = [
         name: 'About',
         path: '/about',
         component: AboutView,
+    },
+    {
+        name: 'PostList',
+        path: '/posts',
+        component: PostListView,
+    },
+    {
+        name: 'PostCreate',
+        path: '/posts/create',
+        component: PostCreateView,
+    },
+    {
+        name: 'PostDetail',
+        path: '/posts/:id',
+        component: PostDetailView,
+    },
+    {
+        name: 'PostEdit',
+        path: '/posts/:id/edit',
+        component: PostEditView,
     },
 ];
 
---
+++ src/views/posts/PostEditView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>게시물 수정</h2>
+        <form @submit.prevent>
+            <div class="mb-3">
+                <label for="title">제목</label>
+                <input type="text" class="form-control" id="title" />
+            </div>
+            <div class="mb-3">
+                <label for="contnet">내용</label>
+                <textarea class="form-check-label" id="content" rows="3"></textarea>
+            </div>
+            <div class="pt-4">
+                <button class="btn btn-outline-dark me-2" @click="goDetail">취소</button>
+                <button class="btn btn-primary">수정</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+const router = useRouter();
+const route = useRoute();
+const id = route.params.id;
+const goDetail = () => router.push({ name: 'PostDetail', params: { id } });
+</script>
